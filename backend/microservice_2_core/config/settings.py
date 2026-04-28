c16fae5b1b2b26a1ab639a597209faf418bcaef1
byPronox <bypronoxty@gmail.com>
2026-04-27 21:41:04 -0500

    fix microservice development

--- backend/microservice_2_core/config/settings.py
+++ backend/microservice_2_core/config/settings.py
@@ -1,7 +1,13 @@
 import os
 
 class Settings:
-    DATABASE_URL = os.getenv("DATABASE_URL")
+    _raw_db_url = os.getenv("DATABASE_URL")
+    
+    if _raw_db_url.startswith("postgres://"):
+        DATABASE_URL = _raw_db_url.replace("postgres://", "postgresql://", 1)
+    else:
+        DATABASE_URL = _raw_db_url
+
     SECRET_KEY = os.getenv("SECRET_KEY")
     ALGORITHM = "HS256"
     ACCESS_TOKEN_EXPIRE_MINUTES = 1440
