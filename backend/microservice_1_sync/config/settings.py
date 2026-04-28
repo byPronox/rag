ef222faecf3da30ba88b946c484e283bdae6a7bd
byPronox <bypronoxty@gmail.com>
2026-04-27 22:27:24 -0500

    fix env db for microservice 1

--- backend/microservice_1_sync/config/settings.py
+++ backend/microservice_1_sync/config/settings.py
@@ -1,7 +1,15 @@
 import os
 
 class Config:
-    RABBITMQ_URL = os.getenv("RABBITMQ_URL")
-    DATABASE_URL = os.getenv("DATABASE_URL")
-    QUEUE_NAME = os.getenv("RABBITMQ_QUEUE")
+    RABBITMQ_URL = os.getenv("RABBITMQ_URL", "amqp://localhost:5672")
+    
+    _raw_db_url = os.getenv("DATABASE_URL")
+    if _raw_db_url.startswith("postgres://"):
+        DATABASE_URL = _raw_db_url.replace("postgres://", "postgresql://", 1)
+    else:
+        DATABASE_URL = _raw_db_url
+    
+    QUEUE_NAME = os.getenv("RABBITMQ_QUEUE") if os.getenv("RABBITMQ_QUEUE") else "rag_products_queue"
+    
+    # Modelo de Inteligencia Artificial
     EMBEDDING_MODEL = "all-MiniLM-L6-v2"
