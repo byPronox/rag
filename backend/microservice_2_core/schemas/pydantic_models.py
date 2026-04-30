f823a3cadf0d3815f91c18a87372be5318e4ecb2
byPronox <bypronoxty@gmail.com>
2026-04-30 16:54:27 -0500

    fix error clas microservice2

--- backend/microservice_2_core/schemas/pydantic_models.py
+++ backend/microservice_2_core/schemas/pydantic_models.py
@@ -4,6 +4,12 @@ class UserCreate(BaseModel):
     email: EmailStr
     password: str
 
+class UserLoginResponse(BaseModel):
+    id: int
+    email: str
+    role: str
+    is_active: bool
+
 class Token(BaseModel):
     access_token: str
     token_type: str
