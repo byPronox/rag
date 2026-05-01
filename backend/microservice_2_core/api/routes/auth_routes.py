2597c83337c5ced3016d099f48f0a17b0414b392
byPronox <bypronoxty@gmail.com>
2026-05-01 14:55:11 -0500

    add auth routes librarys

--- backend/microservice_2_core/api/routes/auth_routes.py
+++ backend/microservice_2_core/api/routes/auth_routes.py
@@ -4,7 +4,8 @@ from fastapi.security import OAuth2PasswordRequestForm
 from database.connection import get_db
 from models.schema import User, UserConfig
 from schemas.pydantic_models import UserCreate, UserLoginResponse
-from security.jwt_handler import get_password_hash, verify_password, create_access_token, get_current_user
+from security.jwt_handler import get_password_hash, verify_password, create_access_token
+from api.deps import get_current_user
 
 router = APIRouter()
 
