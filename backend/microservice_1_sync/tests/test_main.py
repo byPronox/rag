8cb7c129e3da7a5d03bc25861c1599e9a108d71a
byPronox <bypronoxty@gmail.com>
2026-06-27 14:47:00 -0500

    patch rabit mq service worker test

--- backend/microservice_1_sync/tests/test_main.py
+++ backend/microservice_1_sync/tests/test_main.py
@@ -1,7 +1,7 @@
 import runpy
 from unittest.mock import patch
 
-@patch("main.start_worker")
+@patch("services.rabbitmq_service.start_worker")
 def test_main_execution(mock_start_worker):
     runpy.run_module("main", run_name="__main__")
     mock_start_worker.assert_called_once()
